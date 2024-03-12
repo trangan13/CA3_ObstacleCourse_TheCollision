5a28643290c669c30989a5b57f0c4ebc0019f5e1
Erik <146882681+trangan13@users.noreply.github.com>
2024-03-12 23:05:34 +0000

    Rebuild in Visual Studio 2022

---
+++ Source/CA3_ObstacleCourse_GB/NewTestClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CA3_OBSTACLECOURSE_GB_API NewTestClass
+{
+public:
+	NewTestClass();
+	~NewTestClass();
+};
